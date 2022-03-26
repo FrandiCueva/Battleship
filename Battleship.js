8dd9b0f231acc0dc7c3f38804c392c348bd5bcfc
Frandi Cueva <101435398+FrandiCueva@users.noreply.github.com>
2022-03-26 19:25:51 -0400

    Modified first if statement and added other comments

--- Battleship.js
+++ Battleship.js
@@ -7,15 +7,16 @@ var guesses = 0;
 var isSunk = false;
 const prompt = require("prompt-sync")();      // Users input command
 
+
 while (isSunk == false){
 
  guess = prompt("Fire at will (chose from 0-6): ");
 
  //compare?
 
- if (guess == false){
+  if (guess < 0 || guess > 6){
 
-    console.log("Miss!");
+    console.log("Miss! Enter a valid number!");
     guess = prompt("Fire at will: ");
 
  }
@@ -30,7 +31,7 @@ while (isSunk == false){
     if (hits == 3){
 
         isSunk == true;
-        console.log("You Sank my Battleship!");
+        console.log("You Sank my Battleship!");      //change "console.log" to "alert" syntax if using it on a webpage otherwise keep console.log for node.js
 
     }
 
