037638460554d371b520259b061cf7814fd3cf83
Frandi Cueva <101435398+FrandiCueva@users.noreply.github.com>
2022-03-26 17:40:10 -0400

    Started Javascript file and also added the html file

---
+++ Battleship.js
@@ -0,0 +1,40 @@
+var location1 = 2;                                // Variable Declarations
+var location2 = 3;
+var location3 = 4;
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
+const prompt = require("prompt-sync")();      // Users input command
+
+while (isSunk == false){
+
+ guess = prompt("Fire at will (chose from 0-6): ");
+
+ //compare?
+
+ if (guess == false){
+
+    console.log("Miss!");
+    guess = prompt("Fire at will: ");
+
+ }
+
+    else {
+    guesses ++;
+
+    if (guess == location1 || guess == location2 || guess == location3){
+
+    hits ++;
+
+    if (hits == 3){
+
+        isSunk == true;
+        console.log("You Sank my Battleship!");
+
+    }
+
+    }
+ }
+
+}
