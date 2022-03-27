bc58fe48495b7a14679af091e76f8fc5e8bf253f
Frandi Cueva <101435398+FrandiCueva@users.noreply.github.com>
2022-03-27 02:12:40 -0400

    Finished the game for only one ship

--- Battleship.js
+++ Battleship.js
@@ -7,35 +7,33 @@ var guesses = 0;
 var isSunk = false;
 const prompt = require("prompt-sync")();      // Users input command
 
+//Use this while loop if your are using Node.js other wise use the code below
+
+console.log("Please Enter a Number from 0-6!");
 
 while (isSunk == false){
-
- guess = prompt("Fire at will (chose from 0-6): ");
-
- //compare?
+ guess = prompt("Fire at will: ");
 
   if (guess < 0 || guess > 6){
-
-    console.log("Miss! Enter a valid number!");
-    guess = prompt("Fire at will: ");
-
+    console.log("Please enter a valid cell number!"); 
  }
-
     else {
     guesses ++;
 
     if (guess == location1 || guess == location2 || guess == location3){
-
-    hits ++;
-
-    if (hits == 3){
-
-        isSunk == true;
-        console.log("You Sank my Battleship!");      //change "console.log" to "alert" syntax if using it on a webpage otherwise keep console.log for node.js
-
-    }
-
-    }
+     console.log("HIT!");
+     hits ++;
+   
+     if (hits == 3){
+      isSunk = true;
+      console.log("You Sank my Battleship!");      
+   }
+ } 
+   else {
+      console.log("MISS!");
+  }
  }
-
 }
+var stats = "You took " + guesses + " guesses to sink the ship " + "which means your shooting accurcacy was " + (3/guesses);1
+console.log(stats);
+
