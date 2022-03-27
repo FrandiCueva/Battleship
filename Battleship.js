10f992e6595caf156343aa056720bf62c7feb996
Frandi Cueva <101435398+FrandiCueva@users.noreply.github.com>
2022-03-27 17:10:07 -0400

    Added a while loop to use for the web browser

--- Battleship.js
+++ Battleship.js
@@ -7,8 +7,9 @@ var guesses = 0;
 var isSunk = false;
 const prompt = require("prompt-sync")();      // Users input command
 
+// Remover /* and */ from both ends to use the corresponding while loop!
 //Use this while loop if your are using Node.js other wise use the code below
-
+/*
 console.log("Please Enter a Number from 0-6!");
 
 while (isSunk == false){
@@ -43,5 +44,32 @@ console.log(stats,"perfect");
 else {
 console.log(stats, "imprecise");
 }
+*/
 
+// Use this while loop code if you are using a webrowser.
 
+while(isSunk == false){
+   guess = prompt("Fire at will! (Chosose from a number 0-6):");
+
+   if (guess < 0 || guess > 6 ){
+      alert("Please Enter a valid cell number!");
+   }
+  else {
+     guesses++;
+
+     if (guess == location1||guess == location2|| guess == location3){
+     alert("HIT!");
+     hits++;
+
+     if (hits == 3){
+        isSunk = true;
+        alert("You sank my battleship!");
+     }
+   }
+     else {
+        alert("MISS!");
+     }
+  }
+}
+var stats = "You took " +guesses+ " guesses to sink the battleship " + "which means your shooting accuracy is " + (3/guesses);
+alert(stats);
